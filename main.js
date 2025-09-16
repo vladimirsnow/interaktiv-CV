047e2ca4fbfe2e6fba7bff73a45b956067172df8
vladimirsnow <vladimirsnowflak2@gmail.com>
2025-09-16 14:52:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const btn = document.querySelector('.theme-toggle');
+    btn.addEventListener('click', () => {
+      document.body.classList.toggle('dark');
+      btn.textContent = document.body.classList.contains('dark') ? 'Светлая тема' : 'Тёмная тема';
+    });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach(el => observer.observe(el));
+
+const form = document.getElementById("contactForm");
+const msg = document.getElementById("formMessage");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (name.length < 2) {
+    msg.textContent = "Имя должно быть не короче 2 символов!";
+    msg.style.color = "red";
+    return;
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email)) {
+    msg.textContent = "Введите корректный email!";
+    msg.style.color = "red";
+    return;
+  }
+
+  if (message.length < 10) {
+    msg.textContent = "Сообщение должно быть не короче 10 символов!";
+    msg.style.color = "red";
+    return;
+  }
+
+  msg.textContent = "Форма успешно отправлена!";
+  msg.style.color = "green";
+  form.reset();
+});
